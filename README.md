31ad5dc0df5d2f76d4eed5684595ac8a8be0c19d
xrlu0929 <xl2474@tc.columbia.edu>
2018-05-06 23:33:11 -0400

    first test

--- README.md
+++
@@ -1,2 +0,0 @@
-# AlgorithmPrep
-Algorithm Preparation
---
+++ findTreeNode/src/testFind.java
@@ -0,0 +1,10 @@
+/**
+ * Created by annaxlu on 5/6/18.
+ */
+public class testFind {
+    public static void main(String[] args) {
+        TreeNode test = new TreeNode(10);
+        System.out.print(test.findTreeNode(test, 2));
+    }
+
+}
