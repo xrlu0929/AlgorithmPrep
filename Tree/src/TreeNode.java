9799f8e9d711ace74cea4222cb3c1b0bf5bce907
xrlu0929 <xl2474@tc.columbia.edu>
2018-05-06 23:47:26 -0400

    test

---
+++ Tree/src/TreeNode.java
@@ -0,0 +1,34 @@
+/**
+ * Created by annaxlu on 5/6/18.
+ */
+public class TreeNode {
+    int key;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(int v) {
+        this.key = v;
+    }
+
+    /*
+        find
+     */
+    /*
+        find recursive
+     */
+    public boolean findTreeNodeRec(TreeNode root, int target){
+//        while(root!=null) {
+//
+//        }
+        return false;
+    }
+    /*
+        find iterative
+     */
+    public boolean findTreeNodeIte(TreeNode root, int target){
+//        while(root!=null) {
+//
+//        }
+        return false;
+    }
+
+}
---
+++ Tree/src/test.java
@@ -0,0 +1,6 @@
+/**
+ * Created by annaxlu on 5/6/18.
+ */
+public class test {
+
+}
